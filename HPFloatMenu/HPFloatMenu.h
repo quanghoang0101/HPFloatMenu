b5cb85bf9804fa2bfa03dd5cce1c7499f0bb0878
Hoang <hoangq@firestak.com>
2019-04-22 21:31:33 +0700

    + Initial project setup

---
+++ HPFloatMenu/HPFloatMenu.h
@@ -0,0 +1,19 @@
+//
+//  HPFloatMenu.h
+//  HPFloatMenu
+//
+//  Created by Hoang on 4/22/19.
+//  Copyright © 2019 Quang Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HPFloatMenu.
+FOUNDATION_EXPORT double HPFloatMenuVersionNumber;
+
+//! Project version string for HPFloatMenu.
+FOUNDATION_EXPORT const unsigned char HPFloatMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPFloatMenu/PublicHeader.h>
+
+
---
+++ HPFloatMenuExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HPFloatMenuExample
+//
+//  Created by Hoang on 4/22/19.
+//  Copyright © 2019 Quang Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
